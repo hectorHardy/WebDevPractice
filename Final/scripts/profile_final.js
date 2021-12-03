ddcb16bd752ae133a89e6d6851d635ce36003459
valkiss024 <81684813+valkiss024@users.noreply.github.com>
2021-12-03 01:02:51 +0000

    Update profile_final.js

--- Final/scripts/profile_final.js
+++ Final/scripts/profile_final.js
@@ -1,6 +1,6 @@
-const tmdbKey = "16c416000d15a81b6baaeb7641ed1b26";
-const imdbKey = "k_km2lsf6q";
-const rapidKey = "8784b1dedbmshe159ceaadd14dc3p1baa6bjsn50195ef5079f";
+const tmdbKey = ""; // ADD TMDB API KEY HERE
+const imdbKey = ""; // ADD IMDB API KEY HERE
+const rapidKey = ""; // ADD RAPIDAPI API KEY HERE
 const posterBasePath = "https://image.tmdb.org/t/p/original";
 const movieId = window.location.search.split("=")[1];
 
