91556d86fe839ba4492b66123ab004425453d0df
Valentin Kiss <vp.kiss@gmail.com>
2021-12-03 11:26:31 +0000

    minor changes to watchlist feature

--- Final/scripts/landing.js
+++ Final/scripts/landing.js
@@ -8,6 +8,8 @@ var new_url = base_url + "movie/upcoming?&page=2&" + api_key ;
 var top_url = base_url + "movie/top_rated?&" + api_key ;
 var img_url = "https://image.tmdb.org/t/p/w500" ;
 
+var watchList = [] ;
+
 window.onload = function(){
     getMovies(api_url, main) ;
     getMovies(new_url, newMovieDiv)
@@ -18,6 +20,7 @@ function getMovies(url, divTag) {
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results) ;
         showMovies(data.results, divTag) ;
+        
     })
 }
 
@@ -28,12 +31,14 @@ function showMovies(data, divTag) {
         var {title, poster_path, vote_average, id} = movie ;
         var movieEl = document.createElement("div");
         movieEl.classList.add("movie");
-        movieEl.innerHTML = `<img src = "${img_url + poster_path}" alt = "${title}">
+        movieEl.innerHTML = `
+                            <img src = "${img_url + poster_path}" alt = "${title}">
 
                             <div class="movie-info">
-                                <h3 id = "${id}">${title}</h3>
+                                <h3 id = "${id}" onclick = getMovieId(id)>${title}</h3>
                                 <span class = "${getColor(vote_average)}">${vote_average}</span>
-                            </div> `     
+                            </div> 
+                            `     
                             
                             divTag.appendChild(movieEl) ;
 })
@@ -51,3 +56,38 @@ function getColor(vote) {
     }
 }
 
+function getMovieId(id) {
+    
+    console.log(id) ;
+
+    //$('#watchList').empty();
+
+    var id_url = base_url + "movie/" + id + "?" + api_key;
+    console.log(id_url) ;
+    $.getJSON(id_url, function (jsondata) {
+        addResultsDiv(jsondata);
+    });
+
+}
+
+function addResultsDiv(jsondata) {
+    console.log(jsondata);
+    
+
+    var title = jsondata.title;
+    var poster = "http://image.tmdb.org/t/p/w500" + jsondata.poster_path;
+    var voteAvg = jsondata.vote_average;
+    var id = jsondata.id ;
+
+    console.log(title + poster + voteAvg) ;
+
+    var newItem = "<div class = " + 'watchlistItem' + " >" +
+            "<img id = 'wListImg' src ='"+ poster +"' alt = " + title + "'>" +
+
+            "<div class='movie-info'>" + 
+                "<h3 id = '" + id + "' onclick = getMovieId(id)>" + title + "</h3>" + 
+                "<span class = " + getColor(voteAvg) + ">" + voteAvg + "</span>" + 
+            "</div></div>"
+              
+            $('#watchList').append(newItem) ;
+}
